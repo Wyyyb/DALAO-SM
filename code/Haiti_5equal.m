dfb3392afd76630c25b79d23de629bbb4e7eb369
Onion <18867543261@163.com>
2018-02-10 16:54:53 +0800

    Haiti.m Find_equal5.m Haiti_equal5.m

---
+++ code/Haiti_5equal.m
@@ -0,0 +1,24 @@
+%factors1=[7.20; 9.25; 8.45; 10]; %Iraq
+%factors1=[9.65; 9.00; 7.80; 9.70]; %Congo
+factors1=[8.00; 8.6; 8.65; 7.70]; %Haiti
+factors=factors1;
+K=[1/2 0 1/3 1/6; 0 2/3 1/6 1/6; 1/2 0 3/8 1/8; 1/4 1/4 1/4 1/4];
+KC=[1/8; 0; 1/2; 1/4];
+C=0.5;
+year=5;
+X=zeros(year);
+Y1=zeros(year);
+
+for i=1:year
+    X(i)=i;
+    Y1(i)=sum(factors);
+    factors=K*factors;    
+end    
+p1=plot(X, Y1, 'r');
+axis([1 year 32 34]);
+
+xlabel('year');
+ylabel('fragility');
+legend('without climate change influence');
+grid();
+

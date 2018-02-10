c74ebc0831c59648ddc6a78995d9cc205676e841
谷松韵 <songyung0626@gmail.com>
2018-02-10 14:29:59 +0800

    figure 3 and 4

--- code/climate.m
+++ code/climate.m
@@ -2,7 +2,7 @@ factors=[7.10; 5.90; 5.53; 5.80];
 K=[1/2 0 1/3 1/6; 0 2/3 1/6 1/6; 1/2 0 3/8 1/8; 1/4 1/4 1/4 1/4];
 KC=[1/8; 0; 1/2; 1/4];
 C=0.5;
-year=10;
+year=5;
 X=zeros(year);
 Y1=zeros(year);
 Y2=zeros(year);
@@ -15,31 +15,31 @@ for i=1:year
     factors=K*factors;    
 end    
 p1=plot(X, Y1, 'r');
-axis([1 year 20 30]);
+axis([1 year 22 30]);
 hold on
 
 factors=[7.10; 5.90; 5.53; 5.80];
 for i=1:year
-    C=rand(1)*2-1;
+%    C=rand(1)*2-1;
     X(i)=i;
     Y2(i)=sum(factors);
     factors=K*factors+KC*C;    
 end    
 p2=plot(X, Y2, 'g');
-axis([1 year 20 30]);
+axis([1 year 22 30]);
 hold on
 
 factors=[7.10; 5.90; 5.53; 5.80];
 for i=1:year
-    C=rand(1)*2-1;
+%    C=rand(1)*2-1;
     X(i)=i;
     Y3(i)=sum(factors)+C;
     factors=K*factors+KC*C;    
 end    
 p3=plot(X, Y3, 'b');
-axis([1 year 20 30]);
+axis([1 year 22 30]);
 hold off
 xlabel('year');
 ylabel('fragility');
-legend([p1(1), p2(2), p3(3)],'without climate change influence','add indirect influence','add direct influence');
+legend([p1(1), p2(2), p3(3)],'without climate influence','add indirect climate influence','add all climate influence');
 

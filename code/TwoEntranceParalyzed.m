3c6e2d0a7e1b243d4faf780a40b8a5620e972ad6
谷松韵 <songyung0626@gmail.com>
2018-02-10 10:52:31 +0800

    add code cal_PMES

--- code/TwoEntranceParalyzed.m
+++
@@ -1,6 +0,0 @@
-syms lamda;
-mu=0.0433;
-k=2*lamda/((ceil(lamda/mu-0.5)+1)*(ceil(lamda/mu-0.5)-1));
-ezplot(k,[0.12,0.7]);
-xlabel('\lambda(s^-^1)');
-ylabel('dL_q/dt');
--- code/drive_test.m
+++
@@ -1,4 +0,0 @@
-y=fzero('test',0.1)  %get the nearest from 0.1 solution of the equation.
-
-
-
